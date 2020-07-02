650d75d9749e3818582ad3df704a333430de697c
Robert Cronin <Virulious@hotmail.com>
2020-07-02 15:06:50 -0400

    Move id to species in state

--- src/components/Species/CreateSpecies.js
+++ src/components/Species/CreateSpecies.js
@@ -15,9 +15,9 @@ class CreateSpecies extends Component {
       species: {
         name: '',
         description: '',
-        genus: ''
-      },
-      createdSpeciesId: null
+        genus: '',
+        id: null
+      }
     }
   }
 
@@ -31,7 +31,7 @@ class CreateSpecies extends Component {
     const { msgAlert, history } = this.props
 
     createSpecies(this.state.species, this.props.user)
-      .then(res => this.setState({ createdSpeciesId: res.data.species._id }))
+      .then(res => this.setState({ id: res.data.species._id }))
       .then(() => msgAlert({
         heading: 'Species created',
         message: messages.createSpeciesSuccess,
