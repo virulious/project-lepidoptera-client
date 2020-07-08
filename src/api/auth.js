076233f545aac5c56eb8881c117c68b403959730
Robert Cronin <Virulious@hotmail.com>
2020-07-07 22:17:57 -0400

    Change url oneSpecies to `/species/${species.id}`

--- src/api/auth.js
+++ src/api/auth.js
@@ -104,7 +104,7 @@ export const createSpecies = (species, user) => {
 
 export const oneSpecies = (species, user) => {
   return axios({
-    url: apiUrl + '/species/' + species.id,
+    url: apiUrl + `/species/${species.id}/`,
     method: 'GET',
     headers: {
       'Authorization': `Token ${user.token}`
